c5c11a2b60b4edefce0ba646dadf46429ae2d805
tsantos <tales.augusto.santos@gmail.com>
2021-12-08 23:03:55 -0300

    Code style fix

--- user-api/src/server.js
+++ user-api/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const port = 4000;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
